a8ecbd0f0c69e449fc741dac190b011baf102141
darioikic <darioikic5@gmail.com>
2024-10-25 21:44:29 +0200

    i did something

--- src/components/Messages.jsx
+++ src/components/Messages.jsx
@@ -1,15 +1,29 @@
 import React from "react";
 import "./Messages.css";
+import { useState, useEffect } from "react";
+
 
 function Messages() {
+
+  const [dropdownVisible, setDropdownVisible] = useState(false);
+
+  const toggleDropdown = () =>{
+    setDropdownVisible((prev)=>!prev);
+  };
+
   return (
-    <div className="messages">
-      <i className="fa-solid fa-envelope"></i> {/* Message icon */}
-      <span></span> {/* Text for messages */}
-    <div>
+    <div className="messages" onClick = {toggleDropdown}>
+      <i className="fa-solid fa-envelope"></i> 
+      <span></span> 
+      {dropdownVisible && (
+    <div className = "dropdown">
+<p>Message</p>
+<p>Message</p>
+<p>Message</p>
     </div>
+  )}
     </div>
-  );
+);
 }
 
 export default Messages;
