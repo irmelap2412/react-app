354bf63b82d5b06bcb51866d04ee4a4383219661
irmelap2412 <irmela.prcic@fet.ba>
2024-10-26 00:27:05 +0200

    Make multicard functionality

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import React from "react";
+
+import React, { useState } from "react";
 import "./App.css";
 import User from "./components/User";
-import "./components/Searchbar.css"
 import Searchbar from "./components/Searchbar";
 import Messages from "./components/Messages";
-import Leftsidebar from "./components/Leftsidebar"
+import Leftsidebar from "./components/Leftsidebar";
 import Rightsidebar from "./components/Rightsidebar";
+import Friendactivity from "./components/Friendactivity"; // Ensure you import all components
+import Friends from "./components/Friends"; // Import other components as needed
+//import Notifications from "./components/Notifications";
+//import Saved from "./components/Saved";
+//import Settings from "./components/Settings";
 
 function App() {
+  const [activeComponent, setActiveComponent] = useState('Friendactivity');
+
+  const renderComponent = () => {
+    switch (activeComponent) {
+      case "Friends":
+        return <Friends />;
+      default:
+        return <Friendactivity />;
+    }
+  };
+
   return (
     <div className="container">
       <div className="navbar">
-        <Searchbar/>
+        <Searchbar />
         <div className="rightNavbar">
-        <Messages/>
-        <User/>
+          <User />
+          <Messages />
         </div>
       </div>
       <div className="main">
-        <div className="mainLeft"><Leftsidebar/></div>
-        <div className="mainRight"><Rightsidebar/></div>
+        <div className="mainLeft">
+          <Leftsidebar activeComponent={activeComponent}
+            setActiveComponent={setActiveComponent}
+          />
+        </div>
+        <div className="mainRight">
+          <Rightsidebar>
+            {renderComponent()}
+          </Rightsidebar>
+        </div>
       </div>
     </div>
   );
