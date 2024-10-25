42b3db1ab107323a377b82512842e130295e5aba
darioikic <darioikic5@gmail.com>
2024-10-25 18:04:07 +0200

    Make frontend

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+      <div className = "navbar">
+        <div className = "searchbar">
+      <i className="fa-solid fa-magnifying-glass"></i></div>
+      </div>
+      <div className = "main">
+        <div className = "mainLeft"></div>
+        <div className = "mainRight"></div>
+      </div>
     </div>
   );
 }
